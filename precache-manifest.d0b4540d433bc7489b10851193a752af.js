a7dd1ed687cec0bc559d86eeab17fae7b5797c00
Mazahar Shaikh <mazahar.shaikh@bfhl.in>
2020-10-10 18:02:47 +0530

    Updates

---
+++ precache-manifest.d0b4540d433bc7489b10851193a752af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4b1479f8d6df367765370abd3b22147",
+    "url": "/Mazzzy/react-login-form/index.html"
+  },
+  {
+    "revision": "bcfedb1923679c43ef93",
+    "url": "/Mazzzy/react-login-form/static/css/main.8f343eae.chunk.css"
+  },
+  {
+    "revision": "f25520b3880fde8b3e79",
+    "url": "/Mazzzy/react-login-form/static/js/2.5a79ccfb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Mazzzy/react-login-form/static/js/2.5a79ccfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcfedb1923679c43ef93",
+    "url": "/Mazzzy/react-login-form/static/js/main.a70ebca1.chunk.js"
+  },
+  {
+    "revision": "50503949036a25e3a9d6",
+    "url": "/Mazzzy/react-login-form/static/js/runtime-main.d9f34c52.js"
+  },
+  {
+    "revision": "d1459307f0754649abdcedefbad9069b",
+    "url": "/Mazzzy/react-login-form/static/media/dumbbell-logo.d1459307.svg"
+  }
+]);
