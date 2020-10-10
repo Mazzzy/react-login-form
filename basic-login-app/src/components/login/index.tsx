decfce9547b845ec2876eec10902a1e051d5f4d1
Mazahar Shaikh <mazahar.shaikh@bfhl.in>
2020-10-10 17:27:00 +0530

    implemented login form with styling and basic flow

--- basic-login-app/src/components/login/index.tsx
+++ basic-login-app/src/components/login/index.tsx
@@ -1,9 +1,49 @@
-import React, { FC } from "react";
+import React, { FC, useState, FormEvent } from "react";
+import { useFormFields } from "../../libs/hooksLib";
+import Logo from "../../assets/dumbbell-logo.svg";
 
 import "./Login.css";
 
 const Login: FC = () => {
-    return <div>Hi</div>;
+    const [fields, handleFieldChange] = useFormFields({
+        email: "",
+        password: "",
+    });
+    function validateForm() {
+        let formIsValid = true;
+
+        return formIsValid;
+    }
+
+    function handleSubmit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        if (validateForm()) {
+            try {
+                setTimeout(() => {
+                    alert("Form submitted");
+                }, 3000);
+            } catch (e) {
+                alert(e.message);
+            }
+        }
+    }
+
+    return (
+        <form onSubmit={handleSubmit} className="custom-form">
+            <div className="form-head">
+                <img className="logo" src={Logo} alt="logo" />
+                <div className="title">Sign in</div>
+            </div>
+            <div className="form-content">
+                <input id="email" name="email" placeholder="email" type="text" value={fields.email} />
+                <input id="password" name="password" placeholder="password" type="password" value={fields.password} />
+                <div className="form-footer">
+                    <a href="#">Create an account</a>
+                    <button className="button">Log in </button>
+                </div>
+            </div>
+        </form>
+    );
 };
 
 export default Login;
--- basic-login-app/src/libs/hooksLib.ts
+++ basic-login-app/src/libs/hooksLib.ts
@@ -1,11 +1,11 @@
 import { useState, ChangeEvent } from "react";
 
-export function useFormFields(initialState: object) {
+export function useFormFields(initialState: any) {
     const [fields, setValues] = useState(initialState);
 
     return [
         fields,
-        function (event: ChangeEvent<HTMLInputElement>) {
+        (event: ChangeEvent<HTMLInputElement>) => {
             setValues({
                 ...fields,
                 [event.target.id]: event.target.value,
