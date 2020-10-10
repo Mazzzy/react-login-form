de10c6c5b47475857c414c76d550b661609c9a9a
Mazahar Shaikh <mazahar.shaikh@bfhl.in>
2020-10-10 17:57:48 +0530

    added pattern based validations

--- basic-login-app/src/components/login/index.tsx
+++ basic-login-app/src/components/login/index.tsx
@@ -18,7 +18,7 @@ const Login: FC = () => {
     const [errors, setErrors] = useState(errorsInitialState);
 
     const validateForm = () => {
-        let errors = {
+        const errors = {
             email: "",
             password: "",
         };
@@ -27,10 +27,26 @@ const Login: FC = () => {
         if (!fields.email) {
             formIsValid = false;
             errors.email = "* Please enter email.";
+        } else {
+            // regular expression for email validation
+            const emailPattern = new RegExp(
+                /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i
+            );
+            if (!emailPattern.test(fields.email)) {
+                formIsValid = false;
+                errors.email = "* Please enter valid email in the format <email>@<domain>.com";
+            }
         }
         if (!fields.password) {
             formIsValid = false;
             errors.password = "* Please enter password.";
+        } else {
+            // regular expression for password validation
+            const passwordPattern = new RegExp(/^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%&]).*$/);
+            if (!fields.password.match(passwordPattern)) {
+                formIsValid = false;
+                errors.password = "* Please enter password of length 8 and at-least 1 special character + upper case";
+            }
         }
         setErrors(errors);
         return formIsValid;
@@ -63,7 +79,8 @@ const Login: FC = () => {
                     type="text"
                     value={fields.email}
                     onChange={handleFieldChange}
-                    className={errors?.email ? "error" : ""}
+                    className={!errors?.email ? "" : "error"}
+                    title={!errors?.email ? "" : errors?.email}
                 />
                 <input
                     id="password"
@@ -72,12 +89,17 @@ const Login: FC = () => {
                     type="password"
                     value={fields.password}
                     onChange={handleFieldChange}
-                    className={errors?.password ? "error" : ""}
+                    className={!errors?.password ? "" : "error"}
+                    title={!errors?.password ? "" : errors?.password}
                 />
                 <div className="form-footer">
                     <a href="#">Create an account</a>
                     <button className="button">Log in </button>
                 </div>
+                <ul>
+                    {errors?.email ? <li>{errors.email}</li> : ""}
+                    {errors?.password ? <li>{errors.password}</li> : ""}
+                </ul>
             </div>
         </form>
     );
